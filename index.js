8dc5cd90cd291adb9ab6c0766f8434fb4d45d6e4
Aaron <53441990+ilikecheesybread@users.noreply.github.com>
2022-02-18 18:08:18 +0000

    This outputs steams current cpu usage, but steam is dying on my end
    Signed-off-by: Aaron <53441990+ilikecheesybread@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,13 @@
+// Instead of going through the code to poke fun at how horrible my code is,
+// submit a PR to the GitHub repo! That will help me write better software in
+// the future for everyone. Cheers!
+// Node
+
+// const nodegui = require("@nodegui/nodegui")
+const si = require("systeminformation")
+var sleep = require("system-sleep");
+
+while (true) {
+      si.processLoad("steam").then((data) => console.log(data));
+      sleep(1 * 1000);
+}
